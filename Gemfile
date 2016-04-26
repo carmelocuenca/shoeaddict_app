4d9e1315f1f733b5a37967315850294514c705af
Carmelo Cuenca <carmelocuenca@hotmail.com>
2016-04-26 16:03:15 +0000

    Added carrierwave direct

--- Gemfile
+++ Gemfile
@@ -34,6 +34,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :test do
   gem 'capybara'
+  # Poltergeist is a driver for Capybara.
+  # It allows you to run your Capybara tests on a headless WebKit...
+  gem 'poltergeist'
+  gem 'phantomjs', :require => 'phantomjs/poltergeist'
+
 end
 
 group :development, :test do
@@ -61,7 +66,9 @@ end
 
 gem 'bootstrap-sass', '~> 3.0'
 gem 'puma'
-gem 'carrierwave'
+# gem 'carrierwave'
+# Process your uploads in the background by uploading directly to S3
+gem 'carrierwave_direct', :git => "git://github.com/dwilkie/carrierwave_direct.git"
 gem 'mini_magick'
 gem "fog"
 
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,20 +1,25 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWaveDirect::Uploader
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
+
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-  storage :fog
+  # storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
+=begin
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+=end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
